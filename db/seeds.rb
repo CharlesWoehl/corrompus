1f891bbb64df0dbe03111466ba938777fffb11d9
Charles Woehl <122440168+CharlesWoehl@users.noreply.github.com>
2023-02-28 10:46:36 +0100

    cgt branche

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# Booking.new([{
+#   date_reservation: "2021-02-27",
+#   user_id: 1,
+#   personnality_id: 1
+# }])
+
+# Personnality.new([{
+#   description: "I am a very good person",
+#   skills: "détournement de fonds, triche, fraude",
+#   category: "politique",
+#   rating: 5,
+#   price: 1000,
+#   date_disponibility: "2021-02-27",
+#   user_id: 1
+# }])
+
+# User.new([{
+#   email: "test@lewagon.com"
+#   encrypted_password: "password"
+#   firstname: "prénom"
+#   lastname: "nom de famille"
+
+# }])
