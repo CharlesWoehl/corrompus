d4048be011ac3d7f07f7412edbcd62e4179d33f1
Charles Woehl <122440168+CharlesWoehl@users.noreply.github.com>
2023-03-01 10:41:32 +0100

    maj

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   end
   resources :bookings
   devise_for :users
+  # root to: "personnalities#index"
   root to: "pages#home"
-  # root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
