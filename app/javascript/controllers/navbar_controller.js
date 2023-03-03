674f3c4aadea9f032ae3830fc9093fe27270af4f
Charles Woehl <122440168+CharlesWoehl@users.noreply.github.com>
2023-03-03 11:50:56 +0100

    maj

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,14 +3,21 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar"
 export default class extends Controller {
   connect() {
-
+    console.log("Hello from navbar_controller.js")
   }
-  updateNavbar()
-  {
-    if (window.scrollY >= window.innerHeight) {
-      this.element.classList.add("navbar-lewagon-white")
+
+  updateNavbar() {
+    console.log(scrollY, "scrollY")
+    console.log(window.innerHeight, "window.innerHeight")
+    if (document.location.href === "http://localhost:3000/"){
+      if (window.scrollY >= window.innerHeight) {
+        this.element.classList.remove("transparent")
+        this.element.classList.add("white")
+      } else {
+        this.element.classList.remove("white")
+      }
     } else {
-      this.element.classList.remove("navbar-lewagon-white")
+      this.element.classList.add("white")
     }
   }
 }
